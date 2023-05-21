34e4275fdcd31c0534b57a4064f6cb2a5afa18a4
edoardoperreca <102378903+edoardoperreca@users.noreply.github.com>
2023-05-21 18:48:29 +0200

    Add files via upload

---
+++ conversione base 10 base 2-9.py
@@ -0,0 +1,12 @@
+n=int(input("inserisci numero intero: "))
+b=int(input("inserisci base da 2 a 9: "))
+m=n
+s=""
+if n==0:
+    print(0)
+else:
+  while n>0:
+    s=str(n%b)+s
+    n=n//b
+
+print("il numero",m,"in base b è",s)
---
+++ funzione ricorsiva.py
@@ -0,0 +1,24 @@
+'''
+def somma(n):
+    if n==1:
+        return 1
+    else:
+        return (n)+somma(n-1)
+x=int(input('inserisci un numero naturale: '))
+print('la somma dei primi ',x,'naturali è: ',somma(x))
+###
+def f(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return f(n-1)+f(n-2)
+x=int(input('inserisci: '))
+print(f(x))
+'''
+def somma(n):
+    if n==2:
+        return 2
+    else:
+        return (n)+somma(n-2)
+x=int(input('inserisci un numero naturale pari: '))
+print('la somma dei primi ',x,'pari è: ',somma(x))
